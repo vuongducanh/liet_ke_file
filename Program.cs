80a154d1b3cd56a0b080d74b90925bd789e00629
vuongducanh <anhvuong730@gamail.com>
2018-03-05 16:06:47 +0700

    update

--- Program.cs
+++ Program.cs
@@ -1,39 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.IO;
-namespace Bai_tap_file_io
-{
-    class Program
-    {
-        public static void tree(string args )
-        {
-            string[] folder=Directory.GetDirectories(args);
-            string[] files=Directory.GetFiles(args);
 
-            if(folder.Length>0){
-                for (int i = 0; i < folder.Length; i++)
-                {
-                    string extension = Path.GetFileName(folder[i]);
-                    args+="\n"+"│"  +"\n"+ "├── "+"["+extension+"]"; 
-                    tree(folder[i]);
-                }
-            }
-            if(files.Length>0){
-                for (int i = 0; i < files.Length; i++)
-                {
-                    string extension = Path.GetFileName(files[i]);
-                    args+="\n"+ "│" +"\n"+ "├──"+extension; 
-                }
-                 System.Console.WriteLine(args); 
-            }
-        }
-        static void Main(string[] args)
-        {
-            string input = "/home/ducanh/Desktop/folder/";
-            System.Console.WriteLine(Path.GetFileName(input));
-            tree(input);
-        }
-    }
+namespace directory
+{
+   class Program
+   {
+    
+
+       static void tree(string args,string padding){
+           string[] path = Directory.GetDirectories(args);
+           string[] files = Directory.GetFiles(args);
+           if (path.Length>0){
+               for (int x = 0; x< path.Length; x++){                    
+                   string current_dir = Path.GetFileName(path[x]);
+                   if (x==path.Length-1){                        
+                       if (files.Length>0){
+                           System.Console.WriteLine(padding+"  ├──"+"["+current_dir+"]");                            
+                           tree(args+"//"+current_dir,padding+"  │  ");
+                       }
+                       else{
+                           System.Console.WriteLine(padding+"  └──"+"["+current_dir+"]");                            
+                           tree(args+"//"+current_dir,padding+"     ");
+                       }
+                   }
+                   else{
+                       System.Console.WriteLine(padding+"  ├──"+"["+current_dir+"]");                      
+                       tree(args+"//"+current_dir,padding+"  │  ");
+                   }                    
+               }
+           }
+           for( int y = 1; y<= files.Length; y++){
+               string current_file = Path.GetFileName(files[y-1]);
+                if (y==files.Length){
+                       System.Console.WriteLine(padding+"  └──"+current_file);                      
+                   }
+                   else{
+                       System.Console.WriteLine(padding+"  ├──"+current_file);                        
+                   }
+           }
+       }
+        static void Main(string[] args)        
+       {                    
+           System.Console.WriteLine("ban  nhap vao duong dan  : ");
+           string input = Convert.ToString(Console.ReadLine());
+            // @"C:\Users\DucAnh\Desktop\testree";
+           System.Console.WriteLine(Path.GetFileName(input));
+           tree(input,"");      
+       }
+   }
 }
